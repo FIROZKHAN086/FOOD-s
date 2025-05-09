8061451809cd4cb658b3d6336c6502a9c884d97a
FIROZ KHAN <computerpc096@gmail.com>
2025-05-09 18:27:38 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,17 @@ const App = () => {
       <Analytics/>
       <Navbar setAuth={setAuth} IsAdmin={isAdmin} />
       {Auth ? <Authpage setAuth={setAuth} /> : <></>}
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5322933904264559"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style={{display:"block"}}
+     data-ad-client="ca-pub-5322933904264559"
+     data-ad-slot="8068549904"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
       <Toaster position="top-left" reverseOrder={true} />
       <Routes>
         <Route path="/" element={<Home setItem={setItem} />} />
